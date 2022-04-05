010ad1dc147934dbb3cc943448e383b541509795
danalex <daniil.alex1@gmail.com>
2022-04-05 19:58:09 +0300

    add connection to database

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
-echo 'test';
+$mysql = mysqli_connect('localhost', 'root', '', 'warehouse');
+if ($mysql->connect_error) {
+    echo 'Error number: ' . $mysql->connect_errno;
+    echo 'Error: ' . $mysql->connect_error;
+} else {
+    echo 'Host info: ' . $mysql->host_info;
+}
